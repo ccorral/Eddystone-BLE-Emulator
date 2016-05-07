b75d3f49b25b0eee04ecc57ae594e50bf67da2c0
Carlos Corral <carloscorral@Carloss-MacBook-Air.local>
2016-05-06 17:33:49 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Eddystone BLE Advertiser for type UID and URL.
+
+This is a small application that emmits Eddystone UID and URL events.
+NOTE: Only available on Chrome OS.
+
+## Screenshot
+![screenshot](/samples/hello-world/assets/screenshot_1280_800.png)
---
+++ main.js
@@ -0,0 +1,90 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  // Center window on screen.
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = 500;
+  var height = 300;
+
+  chrome.app.window.create('index.html', {
+    id: "helloWorldID",
+    outerBounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth-width)/2),
+      top: Math.round((screenHeight-height)/2)
+      }
+    });
+  });
+
+  const eddystone_url = {
+    type: 'eddystone_url',
+    ids: ['https://www.codesmith.io/'],
+    advertisedTxPower: -20
+  };
+  const eddystone_uid = {
+  type: 'eddystone_uid',
+  ids: ['2F234454F4911BA9FFA6', '000000000001'],
+  advertisedTxPower: -20
+  };
+  let adType = $("input[name='eddystone']:checked").val();
+
+  $('#status').append('Not connected');
+
+  // $(document).on('ready', function() {
+  //
+  // });
+
+  $("input[name='eddystone']").on('click', (event) => {
+    // console.log(event);
+    let adTypeText = $("input[name='eddystone']:checked").val();
+    if (adTypeText === 'UID') {
+      adType = eddystone_uid;
+      $('#urlInput').prop("disabled", true);
+    }
+    else {
+      adType = eddystone_url;
+      $('#urlInput').prop("disabled", false);
+    }
+  });
+
+  // Starts Beacon
+  let registered_adv;
+  $("#start").click((event) => {
+      event.preventDefault();
+      let url = $('#urlInput').val();
+
+      if(adType === eddystone_url) {
+          eddystone_url.ids = [url];
+        }
+
+      beacon.registerAdvertisement(adType).then(advertisement => {
+        registered_adv = advertisement;
+        $('#status').text("Connected.");
+        console.log('Advertising: ' + advertisement);
+        $("#stop").prop("disabled", false);
+        $("#stop").css("background-color","red");
+      }).catch(error => {
+          $('#status').text("Issue connecting. Unable to establish an advertisement.");
+          $('#status').append('<br>');
+          $('#status').append(error.message);
+          console.log(error.message)});
+    });
+
+  //Stops Beacon
+  $("#stop").click((event) => {
+      event.preventDefault();
+
+      registered_adv.unregisterAdvertisement().then(() => {
+        $('#status').text("Advertisement unregistered successfully.");
+        $("#stop").css("background-color","#eee");
+          console.log('Advertisement unregistered successfully.');
+      }).catch(error => {
+          console.log('Not connected, therefore could not unregister.');
+          console.log(error.message)});
+  });
